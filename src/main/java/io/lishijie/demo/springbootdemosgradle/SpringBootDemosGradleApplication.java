6eccc302f8ef77207f1ef40355dc0b22d8c48ed0
lishijie-me <2424782003@qq.com>
2024-05-21 19:43:37 +0800

    Initial commit

---
+++ src/main/java/io/lishijie/demo/springbootdemosgradle/SpringBootDemosGradleApplication.java
@@ -0,0 +1,13 @@
+package io.lishijie.demo.springbootdemosgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemosGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemosGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lishijie/demo/springbootdemosgradle/SpringBootDemosGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lishijie.demo.springbootdemosgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemosGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
